1aaca32696e3d0d3f27f9c3726da301b7aee2bfb
krumkp <krumpparvanov@gmail.com>
2026-09-19 02:03:30 +0300

    i fucked up the first pr somehow

--- ising.py
+++ ising.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
+from PyQt6.QtCore import Qt
 
 # In the real world, the values of the energy stored in individual atoms, the Boltzmann constant, and the J-coupling constant are very very small
 # If I was to set the values of these parameters in the code to their real world values, the code would break down
@@ -12,66 +13,75 @@ from pyqtgraph.Qt import QtCore, QtWidgets
 # T here is just an arbitrary "hotness" indicator, taking values between 1 and 15.
 # I have left the parameters themselves in the calculations for physical clarity
 
-# app = QtWidgets.QApplication([])
-# win = QtWidgets.QWidget()
-# win.setWindowTitle('Ising Model Animation')
-# layout = QtWidgets.QVBoxLayout()
-# win.setLayout(layout)
-# plot_widget = pg.PlotWidget()
-# layout.addWidget(plot_widget)
-# img = pg.ImageItem()
-# plot_widget.addItem(img)
+# pyqtgraph and pyqt stuff. i have no idea if im doing things the most optimal way or not. took me basically a whole day to figure this stuff out
 
-# # slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-# # slider.setMinimum(1)
-# # slider.setMaximum(100)
-# # slider.setValue(23)
-# # layout.addWidget(slider)
+app = QtWidgets.QApplication([])
+win = QtWidgets.QWidget()
+win.setWindowTitle('Ising Model Animation')
+win.resize(1600, 800)
+vlayout = QtWidgets.QVBoxLayout()
+hlayout = QtWidgets.QHBoxLayout()
+win.setLayout(vlayout)
+plot = pg.PlotWidget(title='Ising Model Animation')
+plot.setAspectLocked(True)
+hlayout.addWidget(plot)
+img = pg.ImageItem()
+plot.addItem(img)
+colors = [(0, 0, 255, 255),(255, 0, 0, 255)] # -1 is blue, +1 is red 
+map = pg.ColorMap(pos=[1.0, 1.0], color=colors)
+img.setColorMap(map)
 
-Kb = 1 
-J = 1
-size = int(input("Size of square grid: "))
-a = [-1, 1]
-T = 8
-grid = np.random.choice(a, size=(size, size))
-i = np.random.randint(0, high=(size))
-j = np.random.randint(0, high=(size))
+# slider for real-time temperature adjustment
+sld = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+sld.setRange(1,100)
+sld.setValue(40)
+label = QtWidgets.QLabel(f"Temperature: {sld.value() / 25}")
 
-print(i, j, grid, grid[i,j], sep='\n')
+# graph that shows the current magnetization (average value of all the grid's -1s and +1s)
+graph = pg.PlotWidget(title='Magnetization')
+graph.setYRange(-1, 1)
+mag_curve = graph.plot(pen='g')
+hlayout.addWidget(graph)
 
-try:
-    up = grid[i-1,j]
-except IndexError:
-    up = 0
+vlayout.addLayout(hlayout)
 
-try:
-    down = grid[i+1,j]
-except IndexError:
-    down = 0
+vlayout.addWidget(sld)
+vlayout.addWidget(label)
 
-try:
-    left = grid[i,j-1]
-except IndexError:
-    left = 0
+# tired of dealing with input() prompts, i feel like 100 is a good size for a grid
+size = 100
+grid = np.random.choice([-1, 1], size=(size, size))
+mag_data = np.zeros(500)
 
-try:
-    right = grid[i,j+1]
-except IndexError:
-    right = 0
+def update():
+    global grid
+    Kb = 1
+    J = 1
+    T = sld.value() / 25
+    label.setText(f"Temperature: {T:.1f}")
+    for _ in range(1500):
+        i = np.random.randint(0, size)
+        j = np.random.randint(0, size)
+        up = (i - 1) % size # Previously, i was using try and except, in case of i or j being 0. that felt kind of stupid, was probably slowing down the program
+        down = (i + 1) % size # One thing about this modulo method is, it makes the grid wrap around, kind of like a torus.
+        left = (j - 1) % size # The elements on the edges are "aligned" with the opposite end of the grid
+        right = (j + 1) % size # Technically, that isn't how the system "physically" works, but I feel like it looks more elegant.
+        delta_E = 2*J*(grid[up,j] + grid[down,j] + grid[i,left] + grid[i,right])*grid[i,j]
+        if delta_E <= 0:
+            grid[i,j] *= -1
+        else:
+            if math.exp(-(delta_E)/(Kb*T)) > np.random.rand():
+                grid[i,j] *= -1
+            else:
+                None
+    img.setImage(grid)
+    current_mag = np.sum(grid) / (size*size)
+    mag_data[:-1] = mag_data[1:]
+    mag_data[-1] = current_mag
+    mag_curve.setData(mag_data)
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(16)
 
-delta_E = 2*J*(up + down + left + right)*grid[i,j]
-res = 'no flip'
-if delta_E <= 0:
-    grid[i,j] = grid[i,j]*-1
-    res = 'flip1' # Flipping, because it would lower the overall energy of the system
-else:
-    if math.exp(-(delta_E)/(Kb*T)) - np.random.rand() > 0:
-        grid[i,j] = grid[i,j]*-1
-        res = 'flip2' # Flipping brought on by thermal fluctuations
-    else:
-        None
-
-print(up, down, left, right, delta_E, grid, res, sep='\n')
-
-# win.show()
-# app.exec()
+win.show()
+app.exec()
